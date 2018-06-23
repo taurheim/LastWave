da60cbc7dd49d9495b2051517f3ce882b63a3cbd
Niko Savas <niko@savas.ca>
2018-06-23 10:32:45 -0700

    First graph just got drawn

--- renderers/d3-wave.js
+++ renderers/d3-wave.js
@@ -34,7 +34,10 @@ function WaveGraph() {
         /*
             [{
                 color: "#ffffff",
-                data: [(13)],
+                data: [{
+                    x: <int, which time segment?>
+                    y: <int, count in that time segment>
+                }],
                 name: "Caribou"
             }]
         */
@@ -42,19 +45,39 @@ function WaveGraph() {
         for(var i = 0; i < data.length; i++) {
             var dataPoint = data[i];
             var title = dataPoint.title;
-            var counts = dataPoint.counts;
             var color = schemeColors[currentColor++ % colorCount];
+
+            var counts = dataPoint.counts;
+            var rickshawSeriesData = [];
+            for (var j = 0; j < counts.length; j++) {
+                rickshawSeriesData.push({
+                    x: j,
+                    y: counts[j],
+                });
+            }
 
             rickshawData.push({
                 name: title,
-                data: counts,
+                data: rickshawSeriesData,
                 color: color,
             });
         }
 
         // graph = new Rickshaw.Graph()
+        var graph = new Rickshaw.Graph({
+            element: $("#visualization")[0],
+            width: 1000,
+            height: 600,
+            renderer: 'area',
+            offset: 'silhouette',
+            stroke: true,
+            preserve: true,
+            series: rickshawData,
+            fill: "#000000",
+        });
 
         // Beautify the graph
+        graph.render();
 
         // Add month names
 
