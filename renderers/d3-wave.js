0cf7a1cb505ff5bc091466b26df9d7fef15c04ba
Niko Savas <niko@savas.ca>
2018-06-25 15:46:04 -0700

    Added function to find label points on a ripple

--- renderers/d3-wave.js
+++ renderers/d3-wave.js
@@ -1,5 +1,10 @@
+// $.getScript('renderers/d3-wave-algorithms/')
+
 function WaveGraph() {
     this.title = "Wave Graph";
+    
+    // Config
+    this.minimumSegmentsBetweenLabels = 4;
 
     this.getOptions = function() {
         return {
@@ -30,7 +35,7 @@ function WaveGraph() {
         var colorCount = schemeColors.length;
         var currentColor = 0;
 
-        // Parse data into rickshaw format
+        // Parse ripple data into rickshaw format
         /*
             [{
                 color: "#ffffff",
@@ -63,7 +68,7 @@ function WaveGraph() {
             });
         }
 
-        // graph = new Rickshaw.Graph()
+        // Create the wave graph using Rickshaw/d3
         var graph = new Rickshaw.Graph({
             element: $("#visualization")[0],
             width: 1000,
@@ -75,12 +80,66 @@ function WaveGraph() {
             series: rickshawData,
             fill: "#000000",
         });
-
-        // Beautify the graph
         graph.render();
+
+        // Add ripple labels (e.g. Artist Names)
+        this.addGraphLabels();
 
         // Add month names
 
         // Add watermark
+    }
+
+    /*
+        Heart of the LastWave algorithm
+    */
+    this.addGraphLabels = function() {
+        console.log("Adding labels to graph...");
+    }
+
+    /*
+        A "Label Point" is where we are adding a label on a ripple.
+        @param list of counts for a ripple
+        @return list of indices, each one should have a label
+    */
+    this.findLabelPoints = function(rippleCounts) {
+        // Possible points is a list of numbers representing the indices
+        // in data.count that are being considered as label points
+        var possiblePoints = [];
+        for (var i = 0; i < rippleCounts.length; i++) {
+            possiblePoints.push(i);
+        }
+
+        // These are the points we're actually going to use
+        var rippleLabelPoints = [];
+        while (possiblePoints.length !== 0) {
+            // Find max point
+            var maxValue = 0;
+            var maxIndex = 0;
+            for (var i = 0; i < possiblePoints.length; i++) {
+                var index = possiblePoints[i];
+                var value = rippleCounts[index];
+                if (value > maxValue) {
+                    maxValue = value;
+                    maxIndex = index;
+                }
+            }
+
+            if (maxValue === 0) break;
+
+            rippleLabelPoints.push(maxIndex);
+
+            // Remove the nearby indices from possiblePoints
+            var removeFrom = maxIndex - this.minimumSegmentsBetweenLabels;
+            var removeTo = maxIndex + this.minimumSegmentsBetweenLabels;
+            for(var r = removeFrom; r < removeTo; r++) {
+                var index = possiblePoints.indexOf(r);
+                if (index > -1) {
+                    possiblePoints.splice(index, 1);
+                }
+            }
+        }
+
+        return rippleLabelPoints;
     }
 }
