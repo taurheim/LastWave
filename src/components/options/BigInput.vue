58daa6cf19cff8ba4e6e60b1bbff9e884467d391
Niko Savas <niko@savas.ca>
2018-08-01 09:32:06 -0700

    Added scrobblify plug

--- src/components/options/BigInput.vue
+++ src/components/options/BigInput.vue
@@ -1,5 +1,10 @@
 <template>
-  <input class="big-input" v-model="currentValue" :placeholder="optionData.title">
+  <span>
+    <input class="big-input" v-model="currentValue" :placeholder="optionData.title">
+    <div class="scrobblify-plug">
+      <a href="https://savas.ca/scrobblify">I only have a Spotify account</a>
+    </div>
+  </span>
 </template>
 <style scoped>
 /* TODO use SCSS */
@@ -15,6 +20,9 @@
 }
 .big-input:focus {
   border: 1px solid rgba(64, 196, 255, 1);
+}
+.scrobblify-plug {
+  font-size: 12px;
 }
 </style>
 <script lang="ts">
